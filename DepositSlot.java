00ba5c97d21618fc1359930f8a0c4d80ebe3aef9
Brainless-Loco <tonmoydas961179@gmail.com>
2022-05-09 21:43:46 +0600

    Finalized Implementation

---
+++ DepositSlot.java
@@ -0,0 +1,15 @@
+// DepositSlot.java 
+// Represents the deposit slot of the ATM
+
+public class DepositSlot 
+{
+    // indicates whether the envelope was received
+    // (always returns true, because this is only a software simulation of a real deposit slot)
+    public boolean isEnvelopeReceived()
+    {
+        return true;
+    }// end method isEnvelopeReceived
+}// end class DepositSlot
+
+
+/* Copywrite: Brainless-Loco */
---
+++ Main.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+    ATM theATM = new ATM();
+    theATM.run();
+  }
+}
+
+
+/* Copywrite: Brainless-Loco */
+/* Github: https://github.com/Brainless-Loco */
